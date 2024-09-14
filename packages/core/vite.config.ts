8d408cb391af803df511c53552d047520dff1274
jiko21 <sos.daiki.keion@gmail.com>
2024-09-14 15:48:41 +0900

    :+1: add core package

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitest/config';
+import packageJson from './package.json';
+
+export default defineConfig({
+    test: {
+        name: packageJson.name,
+        dir: './src',
+        watch: false,
+        environment: 'node',
+        coverage: {
+            enabled: true,
+            provider: 'istanbul',
+            include: ['src/**/*'],
+        },
+        typecheck: {
+            enabled: true,
+        },
+        restoreMocks: true,
+    }
+});
